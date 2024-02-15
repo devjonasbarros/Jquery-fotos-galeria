64d394380bbc7ef19763af58623f039a8997361a
jonas Barros <jangelocc@gmail.com>
2024-02-15 07:16:12 -0300

    first commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function(){
 
-    console.log(document.querySelector('headrt button'))
+    console.log(document.querySelector('header button'))
     console.log($("#botao-cancelar"))
 
 })
